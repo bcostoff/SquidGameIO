d92e266ce7a48c8767c14a006259080ecc2ce379
Beau Costoff <wbeaumont@rogers.com>
2021-11-02 15:06:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, 'public');
-const PORT = process.env.PORT || 5000;
+const PORT = 3000 || process.env.PORT;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
@@ -16,17 +16,120 @@ let Player = require('./server/Player');
 let players = [];	// Array of connected players
 let num_of_players = 0 // Player count
 let started = false
+let roomList = [];
 
 //Run when client connects
 io.on('connection', socket => {
     // console.log('New Squid Game Connection: ' + socket.id);
+    io.emit('list rooms', socket.rooms);
+
+    // console.log(socket.rooms);
 
     //Welcome current user
     socket.emit('message', 'Welcome to Squid Game');
 
     num_of_players++;
-    io.emit('player count', { num_of_players: num_of_players })
 
+    socket.on('create room', function (room) {
+        try {
+            socket.join(room, function() {
+                // console.log("Socket now in rooms", socket.rooms);
+                // io.to(room).emit('user joined', socket.id);
+
+                var currentPlayer = playerById(socket.id);
+
+                // Player not found
+                if (!currentPlayer) {
+                    console.log('Player not found: ' + socket.id)
+                    return
+                }
+
+                var clients = io.sockets.adapter.rooms[room].sockets;
+                var numClients = clients ? Object.keys(clients).length : 0;
+
+                currentPlayer.setRoom(room);
+                currentPlayer.setName(numClients);
+
+                var r = { roomName: room, locked: false, numClients: numClients }
+                roomList.push(r);
+                io.emit('list rooms', roomList);
+                io.to(room).emit('player count', { numClients: numClients })
+                socket.emit('join room', { room: room });
+            });
+        }catch(e){
+            console.log('[error]','join room :',e);
+            socket.emit('error','couldnt perform requested action');
+        }
+    });
+
+
+    socket.on('join room', function (room) {
+        try {
+            socket.join(room, function() {
+                // console.log("Socket now in rooms", socket.rooms);
+                // io.to(room).emit('user joined', socket.id);
+
+                var currentPlayer = playerById(socket.id);
+
+                // Player not found
+                if (!currentPlayer) {
+                    console.log('Player not found: ' + socket.id)
+                    return
+                }
+
+                var clients = io.sockets.adapter.rooms[room].sockets;
+                var numClients = clients ? Object.keys(clients).length : 0;
+
+                currentPlayer.setRoom(room);
+                currentPlayer.setName(numClients);
+
+                var r = { roomName: room, locked: false, numClients: numClients }
+                roomList.push(r);
+                io.emit('list rooms', roomList);
+                io.to(room).emit('player count', { numClients: numClients })
+                socket.emit('join room', { room: room });
+            });
+        }catch(e){
+            console.log('[error]','join room :',e);
+            socket.emit('error','couldnt perform requested action');
+        }
+    });
+
+
+    socket.on('leave room', function (room) {
+        try {
+            // console.log(room)
+            // socket.to(room).emit('user left', socket.id);
+            
+            var currentPlayer = playerById(socket.id);
+
+            // Player not found
+            if (!currentPlayer) {
+                console.log('Player not found: ' + socket.id)
+                return
+            }
+
+            // var clients = io.sockets.adapter.rooms[room].sockets;
+            // var numClients = clients ? Object.keys(clients).length : 0;
+            // var obj = getByValue(roomList, room);
+
+
+            currentPlayer.setRoom('');
+            currentPlayer.setName('');
+
+            socket.leave(room);
+
+            io.emit('list rooms', roomList);
+            io.to(room).emit('player count', { numClients: 1 })
+            socket.emit('leave room', { id: socket.id, numClients: 1 });
+            
+        }catch(e){
+            console.log('[error]','leave room :',e);
+            socket.emit('error','couldnt perform requested action');
+        }
+    });
+
+    
     //All clients but user
     // socket.broadcast.emit();
 
@@ -36,6 +139,18 @@ io.on('connection', socket => {
     //Runs when client disconnects
     socket.on('disconnect', () => {
         num_of_players--;
+
+        // var currentPlayer = playerById(socket.id);
+
+        // // Player not found
+        // if (!currentPlayer) {
+        //     console.log('(Disconnect)Player not found: ' + socket.id)
+        //     return
+        // }
+
+        // var clients = io.sockets.adapter.rooms[currentPlayer.getRoom()].sockets;
+        // var numClients = clients ? Object.keys(clients).length : 0;
+
         var removePlayer = playerById(socket.id)
 
         // Player not found
@@ -44,11 +159,11 @@ io.on('connection', socket => {
             return
         }
 
-        // Remove player from players array
+        // // Remove player from players array
         players.splice(players.indexOf(removePlayer), 1)
 
-        // Broadcast removed player to connected socket clients
-        socket.broadcast.emit('remove player', {id: socket.id, num_of_players: num_of_players})
+        // // Broadcast removed player to connected socket clients
+        socket.broadcast.to(removePlayer.getRoom()).emit('remove player', { id: socket.id, numClients: 1 })
     })
 
     socket.on('new player', data => {
@@ -73,6 +188,13 @@ io.on('connection', socket => {
     })
 
     socket.on('start game', () => {
+        var currentPlayer = playerById(socket.id);
+
+        // Player not found
+        if (!currentPlayer) {
+            console.log('Player not found: ' + socket.id)
+            return
+        }
         if (!started) {
             duration = 60;
             var timer = duration, minutes, seconds;
@@ -83,7 +205,7 @@ io.on('connection', socket => {
                 minutes = minutes < 10 ? "0" + minutes : minutes;
                 seconds = seconds < 10 ? "0" + seconds : seconds;
 
-                io.emit('update time', { minutes: minutes, seconds: seconds })
+                io.to(currentPlayer.getRoom()).emit('update time', { minutes: minutes, seconds: seconds })
 
                 if (--timer < 0) {
                     timer = duration;
@@ -94,15 +216,25 @@ io.on('connection', socket => {
     })
 
     socket.on('player eliminated', data => {
-        io.emit('player eliminated', { id: data.id })
+        var currentPlayer = playerById(socket.id);
+
+        // Player not found
+        if (!currentPlayer) {
+            console.log('Player not found: ' + socket.id)
+            return
+        }
+        io.to(currentPlayer.getRoom()).emit('player eliminated', { name: currentPlayer.getName() })
     })
 
     socket.on('flip switch', data => {
-        io.emit('flip switch', { go: data.go })
-    })
+        var currentPlayer = playerById(socket.id);
 
-    socket.on('lobby joined', () => {
-        // io.emit('message', 'A new player has joined.');
+        // Player not found
+        if (!currentPlayer) {
+            console.log('Player not found: ' + socket.id)
+            return
+        }
+        io.to(currentPlayer.getRoom()).emit('flip switch', { go: data.go })
     })
 
     socket.on('move player', data => {
@@ -121,7 +253,7 @@ io.on('connection', socket => {
         movePlayer.setAlive(data.alive)
 
         // Broadcast updated position to connected socket clients
-        socket.broadcast.emit('move player', {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY(), alive: movePlayer.getAlive()})
+        socket.broadcast.to(movePlayer.getRoom()).emit('move player', {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY(), alive: movePlayer.getAlive()})
     })
 
     socket.on('remove player', () => {
@@ -137,7 +269,7 @@ io.on('connection', socket => {
         players.splice(players.indexOf(removePlayer), 1)
 
         // Broadcast removed player to connected socket clients
-        socket.broadcast.emit('remove player', {id: socket.id, num_of_players: num_of_players})
+        socket.broadcast.to(removePlayer.getRoom()).emit('remove player', {id: socket.id, num_of_players: num_of_players})
     })
     
 });
@@ -179,3 +311,10 @@ server.listen(PORT, () => console.log(`Server is up on port ${PORT}.`))
 //     // socket.so
 // }
 
+function getByValue(arr, value) {
+
+    for (var i=0, iLen=arr.length; i<iLen; i++) {
+  
+      if (arr[i].b == value) return arr[i];
+    }
+  }
