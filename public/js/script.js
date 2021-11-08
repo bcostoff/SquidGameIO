1eb6e585dc68c9c85b2a60770197fa6460f1be43
Beau Costoff <wbeaumont@rogers.com>
2021-11-08 13:49:10 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -93,7 +93,7 @@ socket.on('new player', data => {
   }
 
   //ADD EXISTING PLAYERS TO OPPONENTS ARRAY
-  var opp = new Player(player.x - 100, data.y, 8, 8, "opponent", data.id);
+  var opp = new Player(data.x, data.y, 8, 8, "opponent", data.id);
   var rand = Math.ceil(Math.random() * 10);
   if (rand > 5) {
     opp.shape = 'rect';
@@ -101,7 +101,7 @@ socket.on('new player', data => {
     opp.shape = 'circ';
   }
   opponents.push(opp)
-  console.log(opponents);
+  // console.log(opponents);
 })
 
 
